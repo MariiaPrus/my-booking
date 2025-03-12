5a5cde721b03e2c46df5cd9ce1606d60534229f7
MariiaPrus <prusmaria09@gmail.com>
2025-03-12 14:57:45 +0100

    CSS styling applied

--- my-app-booking/src/components/Form.js
+++ my-app-booking/src/components/Form.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { Container, TextField, MenuItem, Button } from "@mui/material";
 import axios from "axios";
-
+import './Form.css'
 const Form = () => {
   const [destinations, setDestinations] = useState([]);
   const [formData, setFormData] = useState({ name: "", email: "", destination: "" });
@@ -26,17 +26,17 @@ const Form = () => {
 
   return (
     <Container>
-      <form onSubmit={handleSubmit}>
-        <TextField label="Name" name="name" required fullWidth onChange={handleChange} />
-        <TextField label="Email" name="email" required fullWidth type="email" onChange={handleChange} />
-        <TextField select label="Destination" name="destination" required fullWidth onChange={handleChange}>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <TextField className="form-field" label="Name" name="name" required fullWidth onChange={handleChange} />
+        <TextField className="form-field" label="Email" name="email" required fullWidth type="email" onChange={handleChange} />
+        <TextField className="form-field" select label="Destination" name="destination" required fullWidth onChange={handleChange}>
           {destinations.map(dest => (
             <MenuItem key={dest.id} value={dest.label}>
               {dest.label}
             </MenuItem>
           ))}
         </TextField>
-        <Button type="submit" variant="contained" color="primary">
+        <Button className="form-button" type="submit" variant="contained" color="primary">
           Send
         </Button>
       </form>
